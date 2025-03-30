1edac7d2541cc37c0a86d98e6a933454fec43960
Admin <tranhuuphuc.k20.cva@gmail.com>
2025-03-30 16:40:15 +0700

    Testing merge

--- src/main/java/com/ProjectMovie/MovieBackendSpringBootApplication.java
+++ src/main/java/com/ProjectMovie/MovieBackendSpringBootApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MovieBackendSpringBootApplication {
+    
+
     public static void main(String[] args) {
         SpringApplication.run(MovieBackendSpringBootApplication.class, args);
+        System.out.println("I have write it to be testing merge !");
     }
-
 }
